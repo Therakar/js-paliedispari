fc51b6a4dff138493428568fbb2d098adfbb4ab2
Therakar <matteo.pau200@gmail.com>
2022-10-28 19:19:26 +0200

    assegna a una variabile la somma dei due numeri

--- js-pariedispari/js/main.js
+++ js-pariedispari/js/main.js
@@ -1,11 +1,14 @@
 "use strict"
 
 //chiedo all'utente se sceglie pari o dispari
-const userEvenOdd = prompt("Pari o dispari?") ;
+const userEvenOdd = prompt("Pari o dispari?").toLowerCase;
 
 //chiedo all'utente di inserire un numero da 1 a 5
 const numberUser =  Number(prompt("scegli un numero da 1 a 5"));
 
 // genero un numero random che sarà la "scelta" del computer
 const numberIA = Math.floor(Math.random()* 5) + 1;
+
+// assegno a una variabile la somma dei due numeri
+let sum = numberUser + numberIA;
 
