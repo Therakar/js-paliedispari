d7cdccf2ef869f118988b9d4e4a634533111adc6
Therakar <matteo.pau200@gmail.com>
2022-10-28 19:09:50 +0200

    elimina la funzione creata in precendenza e inserisce due prompt : -uno che chiede all'utente di inserire "pari" o "dispari" -uno che chiede di inserire un numero da uno a 5
    genera un numero casulae che sarà il numero scelto dall'IA

--- js-pariedispari/js/main.js
+++ js-pariedispari/js/main.js
@@ -1,7 +1,11 @@
 "use strict"
 
-// funzione che genera un numero random 
-function getRndInteger (min, max){
-    return Math.floor(Math.random()*(max-min + 1) + min)
-} 
+//chiedo all'utente se sceglie pari o dispari
+const userEvenOdd = prompt("Pari o dispari?") ;
+
+//chiedo all'utente di inserire un numero da 1 a 5
+const numberUser =  Number(prompt("scegli un numero da 1 a 5"));
+
+// genero un numero random che sarà la "scelta" del computer
+const numberIA = Math.floor(Math.random()* 5) + 1;
 
