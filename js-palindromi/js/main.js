a9edc6a6baca2338db33b1a1f8230da87fd6a848
Therakar <matteo.pau200@gmail.com>
2022-10-28 17:35:32 +0200

    inserisce la let che conterrà la parola inserita tramite promptma con l'ordine delle lettere invertito

--- js-palindromi/js/main.js
+++ js-palindromi/js/main.js
@@ -1,5 +1,8 @@
 "use strict"
 
 // richiesta dell'input dell'utente
-
 const userWord = prompt ("Inserisci la tua parola per vedere se è un palindromo:");
+
+// let che conterrà la parola con l'ordine delle lettere invertito
+let controlWord = "";
+
